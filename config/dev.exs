f4b330b6b3bd7c08f1a0c592cc19ed3bb835fe31
James Martinez <jamescmartinez@gmail.com>
2016-05-12 11:19:28 -0700

    Fix Postgres config for dev/test

--- config/dev.exs
+++ config/dev.exs
@@ -24,8 +24,6 @@ config :phoenix, :stacktrace_depth, 20
 # Configure your database
 config :snowball, Snowball.Repo,
   adapter: Ecto.Adapters.Postgres,
-  username: "postgres",
-  password: "postgres",
   database: "snowball_dev",
   hostname: "localhost",
   pool_size: 10
--- config/test.exs
+++ config/test.exs
@@ -12,8 +12,6 @@ config :logger, level: :warn
 # Configure your database
 config :snowball, Snowball.Repo,
   adapter: Ecto.Adapters.Postgres,
-  username: "postgres",
-  password: "postgres",
   database: "snowball_test",
   hostname: "localhost",
   pool: Ecto.Adapters.SQL.Sandbox
