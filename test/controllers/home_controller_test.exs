4928beb38269d76d6709f4fc68495beeb224d4cf
James Martinez <jamescmartinez@gmail.com>
2016-05-12 18:21:38 -0700

    Add controller test for /

---
+++ test/controllers/home_controller_test.exs
@@ -0,0 +1,8 @@
+defmodule Snowball.HomeControllerTest do
+  use Snowball.ConnCase, async: true
+
+  test "GET /" do
+    conn = get conn(), "/"
+    assert text_response(conn, 200) =~ "🚀"
+  end
+end
