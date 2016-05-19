65cc1d0fa37da7ea13b560610763bb8a3043ae28
James Martinez <jamescmartinez@gmail.com>
2016-05-18 21:35:20 -0700

    Use better JSON API style errors, will change later

--- web/views/changeset_view.ex
+++ web/views/changeset_view.ex
@@ -2,7 +2,24 @@ defmodule Snowball.ChangesetView do
   use Snowball.Web, :view
 
   def render("error.json", %{changeset: changeset}) do
-    # TODO: Make this human-readable
-    %{errors: Enum.into(changeset.errors, %{})}
+    errors = Enum.map(changeset.errors, fn {field, detail} ->
+      %{
+        source: %{pointer: "/data/attributes/#{field}"},
+        title: "Invalid Attribute",
+        detail: render_detail(detail)
+      }
+    end)
+
+    %{errors: errors}
+  end
+
+  defp render_detail({message, values}) do
+    Enum.reduce(values, message, fn {key, value}, acc ->
+      String.replace(acc, "%{#{key}}", to_string(value))
+    end)
+  end
+
+  defp render_detail(message) do
+    message
   end
 end
