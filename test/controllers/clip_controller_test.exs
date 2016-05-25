748b7f2a46102c71ce1d08b916e30e43d5107a21
James Martinez <jamescmartinez@gmail.com>
2016-05-25 13:50:28 -0700

    Make clip stream endpoint reflect current production endpoint

--- test/controllers/clip_controller_test.exs
+++ test/controllers/clip_controller_test.exs
@@ -2,10 +2,14 @@ defmodule Snowball.ClipControllerTest do
   use Snowball.ConnCase, async: true
 
   test "GET /clips/stream", %{conn: conn} do
-    clip = insert(:clip)
-    conn = conn |> authenticate(clip.user.auth_token)
+    # TODO: Check pagination
+    insert(:clip) # Random clip, random user, not following, should not exist in stream
+    follow = insert(:follow)
+    my_clip = insert(:clip, user: follow.follower)
+    following_clip = insert(:clip, user: follow.following, created_at: Ecto.DateTime.cast!("2014-04-17T14:00:00Z"))
+    conn = conn |> authenticate(follow.follower.auth_token)
     conn = get conn, clip_path(conn, :index)
-    assert json_response(conn, 200) == [clip_response(clip)]
+    assert json_response(conn, 200) == [clip_response(my_clip), clip_response(following_clip)]
   end
 
   defp clip_response(clip) do
--- web/controllers/clip_controller.ex
+++ web/controllers/clip_controller.ex
@@ -1,12 +1,24 @@
 defmodule Snowball.ClipController do
   use Snowball.Web, :controller
 
-  alias Snowball.Clip
+  alias Snowball.{Clip, Follow}
 
   plug Snowball.Plug.Authenticate when action in [:index]
 
   def index(conn, _params) do
-    clips = Repo.all(Clip)
+    current_user = conn.assigns.current_user
+    # TODO: Optimize these queries into a single query
+    user_ids = Repo.all(
+      from follow in Follow,
+      where: follow.follower_id == ^current_user.id,
+      select: follow.following_id
+    )
+    user_ids = user_ids ++ [current_user.id]
+    clips = Repo.all(
+      from clip in Clip,
+      where: clip.user_id in ^user_ids,
+      order_by: [desc: clip.created_at]
+    )
     render(conn, "index.json", clips: clips)
   end
 end
