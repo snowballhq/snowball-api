1ee58bda8d595452029b747f7ee5b969f84eba56
James Martinez <jamescmartinez@gmail.com>
2016-05-28 18:09:01 -0700

    Clean up create session tests

--- test/controllers/session_controller_test.exs
+++ test/controllers/session_controller_test.exs
@@ -1,23 +1,22 @@
 defmodule Snowball.SessionControllerTest do
   use Snowball.ConnCase, async: true
 
-  test "POST /users/sign-up", %{conn: conn} do
-    password = "somepassword"
+  test "create/2 returns the user with an auth token", %{conn: conn} do
+    password = "password"
     user = insert(:user, %{password_digest: Comeonin.Bcrypt.hashpwsalt(password)})
     conn = post conn, session_path(conn, :create), user: %{email: user.email, password: password}
     assert json_response(conn, 201) == user_auth_response(user)
   end
 
-  test "POST /users/sign-up with invalid password", %{conn: conn} do
-    user = insert(:user, %{password_digest: Comeonin.Bcrypt.hashpwsalt("password")})
-    conn = post conn, session_path(conn, :create), user: %{email: user.email, password: "anotherpassword"}
-    assert json_response(conn, 401) == %{"message" => "Invalid password"}
+  test "create/2 with an invalid email returns an error", %{conn: conn} do
+    user = insert(:user)
+    conn = post conn, session_path(conn, :create), user: %{email: "wrongemail", password: user.password}
+    assert json_response(conn, 401) == %{"message" => "Invalid email"}
   end
 
-  test "POST /users/sign-up with invalid email", %{conn: conn} do
-    password = "somepassword"
-    insert(:user, %{password_digest: Comeonin.Bcrypt.hashpwsalt(password)})
-    conn = post conn, session_path(conn, :create), user: %{email: "wrongemail", password: password}
-    assert json_response(conn, 401) == %{"message" => "Invalid email"}
+  test "create/2 with an invalid password returns an error", %{conn: conn} do
+    user = insert(:user, %{password_digest: Comeonin.Bcrypt.hashpwsalt("password")})
+    conn = post conn, session_path(conn, :create), user: %{email: user.email, password: "wrongpassword"}
+    assert json_response(conn, 401) == %{"message" => "Invalid password"}
   end
 end
